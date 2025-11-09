80ce4a1dfdb4dd741db65146a984290ee47d4ce1
Samantha Brennan <samanthabrennan@usf.edu>
2025-11-09 18:54:57 -0500

    added statusfilter function

--- src/app/components/StatusFilter.jsx
+++ src/app/components/StatusFilter.jsx
@@ -1,2 +1,20 @@
-'use client';
+"use client";
 
+export default function StatusFilter({ value = "All", onChange }) {
+  return (
+    <label className="flex flex-col text-sm">
+      <span className="mb-1">Status</span>
+      <select
+        value={value}
+        onChange={(e) => onChange?.(e.target.value)}
+        className="rounded-md bg-black/30 p-2 order border-gray-700"
+      >
+        <option>All</option>
+        <option>Open</option>
+        <option>In Progress</option>
+        <option>On Hold</option>
+        <option>Resolved</option>
+      </select>
+    </label>
+  );
+}
