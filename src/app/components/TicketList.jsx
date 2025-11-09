52d1ad4c1c3aa153b239e361e72e142307e31bc7
Samantha Brennan <samanthabrennan@usf.edu>
2025-11-09 18:47:29 -0500

    create ticketlist function

---
+++ src/app/components/TicketList.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function TicketList({ tickets = [], onAddToQueue }) {
+  return (
+    <div className="grid gap-4md: grid-cols-3">
+      {/* Ticket cards will map here */}
+      <p className="text-sm text-gray-400">TicketList scaffold</p>
+    </div>
+  );
+}
