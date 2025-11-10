ea9d504adb7662a95fd29a5b349c7a3243d4f0e0
Samantha Brennan <samanthabrennan@usf.edu>
2025-11-09 19:01:54 -0500

    created searchbox function

--- src/app/components/SearchBox.jsx
+++ src/app/components/SearchBox.jsx
@@ -1,1 +1,15 @@
-'use client';
+"use client";
+
+export default function SearchBox({ value = "", onChange }) {
+  return (
+    <label className="flex flex-col text-sm">
+      <span className="mb-1">Searcg</span>
+      <input
+        value={value}
+        onChange={(e) => onChange?.(e.target.value)}
+        placeholder="Search title or description"
+        className="rounded-md bg-black/30 p-2 border border-gray-700"
+      />
+    </label>
+  );
+}
